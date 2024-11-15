69d05ab5188ceb20aedf5c0d04dc8aa375f12450
David-Cabu <davidcaburlotto2@gmail.com>
2024-11-15 22:30:54 +0100

    added wallet method status

--- wallet.h
+++ wallet.h
@@ -1,19 +1,26 @@
+#include <iostream>
+using namespace std;
+
 #ifndef wallet_h
 #define wallet_h
 
-class Wallet{
-    private:
-        double amount;
-    public:
-        ~Wallet(){}
 
-        void takeMoney(double money){
-            amount -= money;
-        }
+class Wallet {
+private:
+    double amount;
+public:
+    ~Wallet() {}
 
-        void addMoney(double money){
-            amount += money;
-        }
+    void takeMoney(double money) {
+        amount -= money;
+    }
+
+    void addMoney(double money) {
+        amount += money;
+    }
+    void status() {
+        cout << "Total money in wallet: " << amount << endl;
+    }
 };
 
 #endif
