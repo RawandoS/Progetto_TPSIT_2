cb5f06d80c88287d7ca80a04a711d5b997e57939
Samuel Elia Cimpoies <cimpoiessamuel@gmail.com>
2024-11-15 22:31:28 +0100

    display bank method

--- account.h
+++ account.h
@@ -1,16 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
 #ifndef account_h
 #define account_h
 
 class Account{
     private:
         double amount;
+        double invested_amount;
     public:
         ~Account(){}
-        void takeMoney(double money) {
+
+        void takeMoney(double money){
             amount -= money;
         }
-        void addMoney(double money) {
+
+        void addMoney(double money){
             amount += money;
+        }
+
+        void status(){
+            cout << "Amount available: " << amount << '\n';
+            cout << "Amount invested: " << invested_amount << '\n';
         }
 };
 
