a108986fd10e87e1b228d4775ee28f458f66778e
simo <simone.riolfo07@gmail.com>
2025-02-24 21:52:05 +0100

    investment refactoring

--- BankUser.java
+++ BankUser.java
@@ -1,10 +1,11 @@
+import java.util.Random;
+
 public class BankUser {
     private String ID;
     private String userName;
     private double balance;
     private double wallet;
     private int timeSpent;
-
 
     public BankUser(String ID, String userName, double balance, double wallet, int timeSpent) {
         this.ID = ID;
@@ -22,23 +23,20 @@ public class BankUser {
         return userName;
     }
 
-    public void takeMoney(double m){
-        if (m > balance || balance <= 0)
-        {
+    public void takeMoney(double m) {
+        if (m > balance || balance <= 0) {
             System.out.println("Insufficient founds. The import could not be withdrew.\n");
-        }else {
+        } else {
             balance -= m;
             wallet += m;
         }
     }
 
     public void addMoney(double m) {
-        if (m > wallet)
-        {
+        if (m > wallet) {
             System.out.println("Insufficient founds. The import could not be deposited.\n");
         }
-        else
-        {
+        else {
             balance += m;
             wallet -= m;
         }
@@ -50,9 +48,73 @@ public class BankUser {
         System.out.println("Time spent: " + timeSpent + '\n');
     }
 
-    void monthBonus()
-    {
+    void monthBonus() {
         wallet += 100;
         System.out.println("\nA month has passed, 100€ have been deposited in your wallet.\n");
+    }
+
+    void investment(double amount, char period, char risk) {
+        if (amount > balance || balance <= 0) {
+            System.out.println("Insufficient founds");
+            return;
+        }
+        balance -= amount;
+        int _period = 0;
+        if (period == 's') {
+            _period = 30;
+        } else if (period == 'm') {
+            _period = 180;
+        } else if (period == 'l') {
+            _period = 360;
+        } else {
+            System.out.println("Invalid period.");
+            return;
+        }
+        double _risk = 0;
+        if (risk == 'l') {
+            _risk = amount + amount * 2;
+        } else if (risk == 'm') {
+            _risk = amount + amount * 5;
+        } else if (risk == 'h') {
+            _risk = amount + amount * 10;
+        } else {
+            System.out.println("Invalid risk.");
+            return;
+        }
+        double _amount = amount;
+        timeSpent += _period;
+        for (int i = 1; i <= _period; i++) {
+            Random rand = new Random();
+            _amount = rand.nextInt(1000000) % _risk;
+            if (rand.nextInt(1000000) % 2 != 0) {
+                _amount *= -1;
+            }
+            if (i % 30 == 0) {
+                this.monthBonus();
+            }
+        }
+        if (risk == 'l' && (_amount < 0.7 * amount)) {
+            _amount = 0.7 * amount;
+        } else if (risk == 'm' && (_amount < 0.4 * amount)) {
+            _amount = 0.4 * amount;
+        }
+        System.out.print("Initial amount invested: ");
+        System.out.print(amount);
+        System.out.println("€");
+        System.out.print("Final amount: ");
+        System.out.print(_amount);
+        System.out.println("€");
+        if (_amount > amount) {
+            System.out.print("Earned amount: ");
+            System.out.print(_amount - amount);
+            System.out.println("€");
+        } else if (_amount < amount) {
+            System.out.print("Lost amount: ");
+            System.out.print(amount - _amount);
+            System.out.println("€");
+        } else {
+            System.out.println("No amount earned");
+        }
+        balance += _amount;
     }
 }
